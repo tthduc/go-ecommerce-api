53ecf39847562430c9ce74afb63dbcf7eb5e92b6
hanettechnology <thanhduc736@gmail.com>
2025-05-06 15:21:01 +0700

    add viper

---
+++ cmd/cli/viper/main.viper.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server struct {
+		Host string `mapstructure:"host"`
+		Port int    `mapstructure:"port"`
+	} `mapstructure:"server"`
+}
+
+func main() {
+	viper := viper.New()
+	viper.AddConfigPath("./config")
+	viper.SetConfigName("local")
+	viper.SetConfigType("yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		panic(err)
+	}
+
+	// Use the config
+	fmt.Println(config.Server.Port)
+}
